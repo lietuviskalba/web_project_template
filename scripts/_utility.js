222c66b64bc39ce27bdc41d29fe477220cdd865e
Mantas Lingaitis <mantasling@gmail.com>
2022-12-19 22:26:45 +0200

    > Utility script added

---
+++ scripts/_utility.js
@@ -0,0 +1,4 @@
+// Easier and a more clear section separation to see on console
+const topicNameSeparator = topicName => console.log("@@@@@\n*", topicName, "*\n@@@@@");
+
+const sectionNameSeparator = sectionName => console.log("###\n ---", sectionName, "\n###");
